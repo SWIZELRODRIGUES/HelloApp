917156980690ff7dd2b82fef75a3e661d52d6330
Swizel-Rodrigues <swizelrodrigues1908@gmail.com>
2019-08-21 15:21:14 +0530

    Fixed issue branch commit

--- src/Hi.java
+++ src/Hi.java
@@ -2,5 +2,6 @@ public class Hi {
     public static void main(String[] args) throws Exception {
         System.out.println("hi greeting .. today is 21 August ");
         System.out.println("Training ends on 16 September");
+        System.out.println("Fixed issues ");
     }
 }
