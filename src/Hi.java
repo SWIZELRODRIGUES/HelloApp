9cf1b61aafd83517a900ba316dc9fb4ddf037ba6
Swizel-Rodrigues <swizelrodrigues1908@gmail.com>
2019-08-21 14:38:51 +0530

    Day 1 added

--- src/Hi.java
+++ src/Hi.java
@@ -1,5 +1,6 @@
 public class Hi {
     public static void main(String[] args) throws Exception {
         System.out.println("hi greeting");
+        System.out.println(" Today is Day 1 training in Git!git");
     }
 }
