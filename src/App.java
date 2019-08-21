47efda392570c90dd0a7b2e5734bad2db17a5fc8
Swizel-Rodrigues <swizelrodrigues1908@gmail.com>
2019-08-21 15:15:05 +0530

    Testing line added in app

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java How are you?");
+        System.out.println("Testing merge commits!");
     }
 }
