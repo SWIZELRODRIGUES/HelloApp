00461574448dacb24be3cdb9b2a7b9fc5fff79ab
Swizel-Rodrigues <swizelrodrigues1908@gmail.com>
2019-08-21 15:16:56 +0530

    Training end message added to Hi

--- src/Hi.java
+++ src/Hi.java
@@ -1,5 +1,6 @@
 public class Hi {
     public static void main(String[] args) throws Exception {
         System.out.println("hi greeting .. today is 21 August ");
+        System.out.println("Training ends on 16 September");
     }
 }
