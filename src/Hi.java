34c995f9cc42f7efbc4223b77c645ba2343ca0f8
Swizel-Rodrigues <swizelrodrigues1908@gmail.com>
2019-08-21 14:35:08 +0530

    Today's date was added

--- src/Hi.java
+++ src/Hi.java
@@ -1,5 +1,5 @@
 public class Hi {
     public static void main(String[] args) throws Exception {
-        System.out.println("hi greeting");
+        System.out.println("hi greeting .. today is 21 August ");
     }
 }
